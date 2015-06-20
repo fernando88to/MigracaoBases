14dbdcf2441b69f55ce094628d3f916c11adebb6
fernando88to <fernando88to@gmail.com>
2015-06-20 16:33:07 -0300

    Crud serie

---
+++ grails-app/domain/mysql/Serie.groovy
@@ -0,0 +1,26 @@
+package mysql
+
+class Serie {
+	Long id
+	String nome
+	/*
+	*1 = Educação Infantil
+	*2 = Ensino Fundamental
+	*3 = Ensino Médio
+	*/
+	Integer tipo
+	Escola escola
+
+    static constraints = {
+    	nome unique:true, blank:false, nullable:false,maxSize:255
+    	tipo nullable:false, inList:[1,2,3]
+    	escola nullable:true
+    }
+
+    static mapping = {
+    	table 'serie'
+    	version true
+    	datasource 'lookup'
+    	id  generator:'identity'    
+    }
+}
